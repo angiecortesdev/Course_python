e60359dcb76463179e4cfd41f2f0dc52dab73748
Angie cortes <angiecortes29@gmail.com>
2023-11-17 11:15:04 -0500

    Funciones

---
+++ BasicoPython/12-Funciones/2-FuncionesCreadas.py
@@ -0,0 +1,8 @@
+def saludar():
+    print("Hola Angie")
+
+
+saludar()
+saludar()
+saludar()
+
---
+++ BasicoPython/12-Funciones/3-FuncionesMatematicas.py
@@ -0,0 +1,20 @@
+import math
+import random
+
+
+print(math.pow(10,2))
+print(math.sqrt(64))
+print(math.isqrt(64))
+print(math.sin(90))
+print(math.ceil(50.6))
+print(math.pi)
+print(math.floor(50.6))
+
+print(random.randint(1, 50))
+print(random.randrange(1, 6))
+
+
+
+
+
+
